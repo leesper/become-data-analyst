b2899a4f761e1600b00867240519ce19b054ebc9
leesper <pascal7718@gmail.com>
2017-07-03 21:52:31 +0800

    daily commit

--- EDA_Course_Materials/lesson6/regression.R
+++ EDA_Course_Materials/lesson6/regression.R
@@ -17,3 +17,26 @@ flight <- read.table(header=T, text=flighttxt)
 ggplot(data=flight, aes(x=miles, y=price)) + geom_point()
 
 cor.test(flight$miles, flight$price)
+
+stemtxt <- '
+stems	activity
+116	60
+117	67
+120	64
+1	8
+52 13
+79 63
+109	63
+27 2
+85 46
+51 27
+78 43
+55 24
+26 10
+39 28
+107 56
+'
+
+stemcells <- read.table(head=T, text=stemtxt)
+
+cor.test(stemcells$stem, stemcells$activity)
