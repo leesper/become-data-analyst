069c3cf00fcee24b7c86306dfb36822d259ee97d
leesper <pascal7718@gmail.com>
2017-07-26 08:45:15 +0800

    commit decision tree

---
+++ ud120_projects/decision_tree/dt_author_id.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+print 'features', len(features_train[0])
+
+
+
+#########################################################
+### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print 'accuracy', accuracy_score(pred, labels_test)
+#########################################################
+
+
