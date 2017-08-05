b7ded77901ceb9f003cc8aa6a62dc80f08ae47fe
leesper <pascal7718@gmail.com>
2017-08-05 10:48:44 +0800

    add feature selection

--- ud120_projects/text_learning/vectorize_text.py
+++ ud120_projects/text_learning/vectorize_text.py
@@ -52,7 +52,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-	    for w in ["sara", "shackleton", "chris", "germani"]:
+	    for w in ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]:
                 parsed = parsed.replace(w, '')
 
             ### append the text to word_data
