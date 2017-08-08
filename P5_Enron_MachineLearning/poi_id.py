73b3c0a808acb7c06f2879d27a73af49c059ffe8
leesper <pascal7718@gmail.com>
2017-08-08 23:11:01 +0800

    tomato commit

--- P5_Enron_MachineLearning/poi_id.py
+++ P5_Enron_MachineLearning/poi_id.py
@@ -10,7 +10,28 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = [
+'poi',
+'bonus',
+'deferral_payments',
+'deferred_income',
+'director_fees',
+'exercised_stock_options',
+'expenses',
+'from_messages',
+'from_poi_to_this_person',
+'from_this_person_to_poi',
+'loan_advances',
+'long_term_incentive',
+'other',
+'restricted_stock',
+'restricted_stock_deferred',
+'salary',
+'shared_receipt_with_poi',
+'to_messages',
+'total_payments',
+'total_stock_value',
+] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -35,11 +56,11 @@ labels, features = targetFeatureSplit(data)
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
