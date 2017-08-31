369f14715b3631c431bb8dfe912f4e6ef4885180
leesper <pascal7718@gmail.com>
2017-08-31 10:47:14 +0800

    occupation visualization

--- P6_Effective_Data_Visualization/js/common.js
+++ P6_Effective_Data_Visualization/js/common.js
@@ -10,5 +10,20 @@ function preprocess(d) {
   if (d.LoanStatus.indexOf("Past Due") !== -1) {
     d.LoanStatus = "Past Due";
   }
+  if (d.Occupation.indexOf("Student") !== -1) {
+    d.Occupation = "Student";
+  } else if (d.Occupation.indexOf("Pilot") !== -1) {
+    d.Occupation = "Pilot";
+  } else if (d.Occupation.indexOf("Police") !== -1) {
+    d.Occupation = "Police";
+  } else if (d.Occupation.indexOf("Engineer") !== -1) {
+    d.Occupation = "Engineer";
+  } else if (d.Occupation.indexOf("Tradesman") !== -1) {
+    d.Occupation = "Tradesman";
+  } else if (d.Occupation.indexOf("Sales") !== -1) {
+    d.Occupation = "Sales";
+  } else if (d.Occupation.indexOf("Driver") !== -1) {
+    d.Occupation = "Driver";
+  }
   return d;
 }
